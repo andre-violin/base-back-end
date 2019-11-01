72b8b31aa5fed6bac5b847fd7f995f84357ffd2a
Andre Violin <andreviolin@gmail.com>
2019-11-01 09:45:57 -0400

    feat: user and session

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b4fb8851d54048595d35f3cae362eab8',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
-routes.get('/', (req, res) => res.json({ message: 'Legal' }));
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
